e424ae1a234c71d71f2058d3de91794069d192fd
hectpumhui <forsoctask@gmail.com>
2022-11-03 21:54:17 +0200

    defrag

--- git1.py
+++ git1.py
@@ -1,8 +1,3 @@
-sflvsl ssdkfm d
-sknjsdnfjsflsdf
-ps/ heello world
-<<<<<<< HEAD
-jsdfjksdfks
-=======
-saaassassaa
->>>>>>> 3ce9279afb3cd7051ddacf76ddc0acb5a375c39f
+hello world
+sjdfsldjjlsd keyword
+sdsdsd
